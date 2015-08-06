d05d2c4eebf0703ed0b2f7e329be0bbe741c52b3
Sonny T <mail@sonnyt.com>
2015-08-06 09:32:56 -0400

    Changed the port

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    port: 3000,
+    port: 5000,
     api: {
         url: 'https://data.sfgov.org/resource/6a9r-agq8.json',
         token: '699vZaLBHUKIlqvb7fTqL3eSR'
